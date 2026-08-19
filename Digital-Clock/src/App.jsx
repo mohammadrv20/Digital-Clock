f5095b46725cd7e55813db8a6e6b3b8c18c35993
Mohammad Hassani <mohammad.rv19@gmail.com>
2026-08-19 18:28:33 +0330

    add style to the digital clock via tailwind

--- Digital-Clock/src/App.jsx
+++ Digital-Clock/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 export default function App(){
   return(
     <>
-    <div className='bg-[url("/public/image.jpg")] h-screen bg-cover'></div>
+    <div className='bg-[url("/public/image.jpg")] h-screen bg-cover flex justify-center items-center'>
+      <h1 className='text-white text-7xl text-center shadow-2xs backdrop-blur-xs p-3 '>00 : 00 : 00</h1>
+    </div>
     </>
   )
 }
