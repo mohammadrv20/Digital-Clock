1625a663403cefe572a504c4172332da7bad4780
Mohammad Hassani <mohammad.rv19@gmail.com>
2026-08-19 18:15:26 +0330

    implement the background image

--- Digital-Clock/src/App.jsx
+++ Digital-Clock/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 export default function App(){
   return(
     <>
-    <h1 className='text-red-500'>Hello World</h1>
+    <div className='bg-[url("/public/image.jpg")] h-screen bg-cover'></div>
     </>
   )
 }
